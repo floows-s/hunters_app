2c7a4fcc2fc69e83a33731af056749d5a9ea6d60
239001 <239001@edu.rocfriesepoort.nl>
2024-02-12 12:26:26 +0100

    dady

---
+++ android_project/app/src/main/java/com/example/hunters/Make_hunt.kt
@@ -0,0 +1,11 @@
+package com.example.hunters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Make_hunt : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_make_hunt)
+    }
+}
