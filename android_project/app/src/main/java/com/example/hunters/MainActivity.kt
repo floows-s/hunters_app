d09d20c17ee899ddc1436ab7487447fba2b5d5f3
239001 <239001@edu.rocfriesepoort.nl>
2024-02-12 14:40:50 +0100

    pu

--- android_project/app/src/main/java/com/example/hunters/MainActivity.kt
+++ android_project/app/src/main/java/com/example/hunters/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.hunters
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    public fun SelectHunt(view: View) {
+        var goToSelectHunt = Intent(this, SelectHunt::class.java)
+        startActivity(goToSelectHunt)
     }
 }
