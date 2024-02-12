273e93ba1e135c6cf82ecb69258be9763d763231
239001 <239001@edu.rocfriesepoort.nl>
2024-02-12 13:45:05 +0100

    new

---
+++ android_project/app/src/main/java/com/example/hunters/End_hunt.kt
@@ -0,0 +1,11 @@
+package com.example.hunters
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class End_hunt : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_end_hunt)
+    }
+}
