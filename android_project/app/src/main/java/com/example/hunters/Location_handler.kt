f6999d60baa5184f6df8d49815eade038fee21a7
floows-s <flvrij03@gmail.com>
2024-02-13 15:43:43 +0100

    cancer

--- android_project/app/src/main/java/com/example/hunters/Location_handler.kt
+++ android_project/app/src/main/java/com/example/hunters/Location_handler.kt
@@ -12,38 +12,20 @@ import com.google.android.gms.location.LocationServices
 import android.Manifest
 
 class Location_handler(var cur_context: Context) : LocationListener {
-    lateinit var cur_loc: Location;
 
-    companion object{
-        private const val PREMISSION_REQUEST_ACCESS_LOCATION = 100;
-    }
+    private var HasGPS:Boolean = false
+    private var loc : Double = 0.0
+    private var lat : Double = 0.0
+
+    public fun hasGPS() :Boolean{ return HasGPS}
+    public fun getLat() : Double {return lat}
+    public fun getLon() : Double {return loc}
 
 
-    private fun checkPermission(): Boolean {
-        if(ActivityCompat.checkSelfPermission(
-                cur_context,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
-            ActivityCompat.checkSelfPermission(cur_context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-        {
-            return true;
-        }else{
-            return false;
-        }
-    }
 
-    private fun request_permission(){
-        ActivityCompat.requestPermissions(
-            cur_context as Activity,
-            arrayOf(
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-            ),
-            PREMISSION_REQUEST_ACCESS_LOCATION
-        )
-    }
 
     private lateinit var locationManager : LocationManager
-    public fun getLocation() {
+    public fun start_cap() {
         locationManager = cur_context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
         if ((ContextCompat.checkSelfPermission(cur_context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
             val locationPermissionCode = 2
@@ -52,20 +34,10 @@ class Location_handler(var cur_context: Context) : LocationListener {
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 0f, this)
     }
 
-    private var HasGPS:Boolean = false
-    private var loc : Double = 0.0
-    private var lat : Double = 0.0
-
-    public fun hasGPS() :Boolean{ return HasGPS}
-
-    public fun getLat() : Double {return lat}
-    public fun getLon() : Double {return loc}
     override fun onLocationChanged(location: Location) {
-        HasGPS = true
-        loc = location.longitude
-        lat = location.latitude
-        Log.i("gps", "la: " + location.latitude.toString());
-        Log.i("gps", "lo: " + location.longitude.toString());
+        HasGPS = true;
+        loc = location.longitude;
+        lat = location.latitude;
     }
 
 
--- android_project/app/src/main/java/com/example/hunters/Make_location.kt
+++ android_project/app/src/main/java/com/example/hunters/Make_location.kt
@@ -1,22 +1,38 @@
 package com.example.hunters
 
 import Location_handler
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.provider.MediaStore
+import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 
 class Make_location : AppCompatActivity() {
-
+    val REQUEST_IMAGE_CAPTURE = 100;
     var loc_han = Location_handler(this);
+
+    // data for location
+    var lat = 0.0;
+    var lon = 0.0;
+    lateinit var bitmap_img: Bitmap;
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_make_location)
-        loc_han.getLocation();
+
+        var button = findViewById<View>(R.id.btn_gps);
+        button.isEnabled = false;
+
+        loc_han.start_cap();
 
         var timer = object: CountDownTimer(10000,500) {
             override fun onTick(p0: Long) {
@@ -42,12 +58,43 @@ class Make_location : AppCompatActivity() {
         }
     }
     private fun FailedToGetGPS(){
-
+        Log.i("gps", "failed to get gps");
     }
 
-    public fun get_location(view: View){
+    public fun capture_location(view: View){
+//        get data
+        lat = loc_han.getLat();
+        lon = loc_han.getLon();
+//         show data
+        var etx_la = findViewById<EditText>(R.id.etx_la);
+        var etx_lo = findViewById<EditText>(R.id.etx_lo);
 
-        var d = loc_han.getLat()
-        var sd = loc_han.getLat()
+        etx_la.setText(lat.toString());
+        etx_lo.setText(lon.toString());
     }
+
+    public fun capture_img(view: View){
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        try {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }catch (e: ActivityNotFoundException){
+            //Toast.makeText(this, "Error: " + e.LocalizedMessage, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        var image_view: ImageView = findViewById(R.id.img_view);
+
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            bitmap_img = data?.extras?.get("data") as Bitmap;
+
+            image_view.setImageBitmap(bitmap_img);
+        }
+        else{
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+
 }
