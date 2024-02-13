45e33602cd5ab84118092c9968c992e290b4cca6
239001 <239001@edu.rocfriesepoort.nl>
2024-02-13 12:45:47 +0100

    y

--- android_project/app/src/main/java/Location_handler.kt
+++ android_project/app/src/main/java/Location_handler.kt
@@ -1,17 +1,70 @@
+import android.app.Activity
 import android.content.Context
-import com.example.hunters.Location
+import android.content.pm.PackageManager
+import android.location.Location
+import android.util.Log
+import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
-class Location_handler(context: Context) {
+class Location_handler(var cur_context: Context) {
 
 
     private lateinit var fused_location_prov_client: FusedLocationProviderClient;
-    init{
-        
+
+    init {
+        fused_location_prov_client = LocationServices.getFusedLocationProviderClient(cur_context);
+
+        if(check_permission()){
+            Log.i("test", "JA");
+        }else{
+            Log.i("test", "JA");
+        }
+
     }
 
-    public fun get_location(){
 
+    public fun get_current_location(): Location{
+        if(checkPermission()){
+            fused_location_prov_client.lastLocation.addOnCompleteListener(cur_context){ task->
+                val location: Location?=task.result
+                Log.i("test". "latitude: " + location.latitude);
+                Log.i("test". "longitude: " + location.longitude);
+
+                return location;
+            }
+        }else{
+            request_permission();
+        }
+    }
+
+
+    companion object{
+        private const val PREMISSION_REQUEST_ACCESS_LOCATION = 100;
+    }
+
+
+    private fun checkPermission(): Boolean {
+        if(ActivityCompat.checkSelfPermission(
+                cur_context,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(cur_context, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private fun request_permission(){
+        ActivityCompat.requestPermissions(
+            cur_context,
+            arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION,
+                PREMISSION_REQUEST_ACCESS_LOCATION
+            )
+        )
     }
 
 }
--- android_project/app/src/main/java/com/example/hunters/SelectHunt.kt
+++ android_project/app/src/main/java/com/example/hunters/SelectHunt.kt
@@ -1,11 +1,51 @@
 package com.example.hunters
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet
+import com.google.rpc.LocalizedMessage
+
 
 class SelectHunt : AppCompatActivity() {
+
+    lateinit var imageView: ImageView
+    lateinit var button: Button
+    val REQUEST_IMAGE_CAPTURE = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_hunt)
+
+        imageView = findViewById(R.id.image_save)
+        button = findViewById(R.id.btn_take_picture)
+
+        button.setOnClickListener {
+            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+            try {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }catch (e: ActivityNotFoundException){
+                //Toast.makeText(this, "Error: " + e.LocalizedMessage, Toast.LENGTH_SHORT).show()
+            }
+        }
+        //android.location.Location.distanceBetween();
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(imageBitmap)
+        }
+        else{
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
