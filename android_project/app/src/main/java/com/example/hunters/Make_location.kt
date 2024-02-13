0385d501f14dae679d52ae7df382419a4cf3a387
239001 <239001@edu.rocfriesepoort.nl>
2024-02-13 13:58:21 +0100

    a

--- android_project/app/src/main/java/com/example/hunters/Make_location.kt
+++ android_project/app/src/main/java/com/example/hunters/Make_location.kt
@@ -1,11 +1,17 @@
 package com.example.hunters
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class Make_location : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_make_location)
+    }
+    public fun BackTo_make_hunt_klaar(view: View){
+        var goToMake_hunt = Intent(this, Make_hunt::class.java)
+        startActivity(goToMake_hunt)
     }
 }
