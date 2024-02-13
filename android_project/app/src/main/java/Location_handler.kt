9b6c70edda9b001a02a17585bfcf91c0885af748
floows-s <flvrij03@gmail.com>
2024-02-13 10:54:31 +0100

    android mani

---
+++ android_project/app/src/main/java/Location_handler.kt
@@ -0,0 +1,17 @@
+import android.content.Context
+import com.example.hunters.Location
+import com.google.android.gms.location.FusedLocationProviderClient
+
+class Location_handler(context: Context) {
+
+
+    private lateinit var fused_location_prov_client: FusedLocationProviderClient;
+    init{
+        
+    }
+
+    public fun get_location(){
+
+    }
+
+}
