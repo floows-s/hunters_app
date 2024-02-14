0a996b7d1dc2fce1730de77d68eee7813bb5f706
floows-s <flvrij03@gmail.com>
2024-02-14 13:47:42 +0100

    ja

--- android_project/app/src/main/java/com/example/hunters/Make_hunt.kt
+++ android_project/app/src/main/java/com/example/hunters/Make_hunt.kt
@@ -1,22 +1,52 @@
 package com.example.hunters
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Layout
+import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
 
 class Make_hunt : AppCompatActivity() {
 
-    var database = Database(this);
+    lateinit var database : Database
     var loc_array = ArrayList<Hunt_location>();
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_make_hunt)
 
-        // get all data
+        database = Database(this);
+
+        // get all data and load all data to page
+//        var btn_layout: LinearLayout = findViewById(R.id.btn_layout);
+//        var new_btn = Button(this);
+//        btn_layout.addView(new_btn);
+//        new_btn.setText("Delete location: bruh");
+
         loc_array = database.get_locations();
-        for (i in 1..5) {
-            println(i)
+
+//        var test = loc_array.get(0).latitude;
+//        var size = loc_array.size;
+//        Log.i("ID TEST", "lat: $test | size: $size");
+
+        var btn_layout: LinearLayout = findViewById(R.id.btn_layout);
+        for (i in 0..loc_array.size) {
+            Log.i("hunt_location", loc_array.get(i).toString());
+            var cur_id = loc_array.get(i).id;
+
+            var new_btn = Button(this);
+            btn_layout.addView(new_btn);
+
+            new_btn.setText("Delete location: " + cur_id.toString());
+            new_btn.setOnClickListener {
+                database.delete_location(cur_id);
+            }
+
         }
     }
 
