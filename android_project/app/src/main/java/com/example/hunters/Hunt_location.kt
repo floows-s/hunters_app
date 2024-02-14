2d9bc99c7015aef26ea2095ebd635fcdb9cba5be
239001 <239001@edu.rocfriesepoort.nl>
2024-02-14 09:47:05 +0100

    help

--- android_project/app/src/main/java/com/example/hunters/Hunt_location.kt
+++ android_project/app/src/main/java/com/example/hunters/Hunt_location.kt
@@ -5,7 +5,7 @@ import android.health.connect.datatypes.ExerciseRoute.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class Hunt_location(var latitude: Double, var longitude: Double, var img_bitmap: Bitmap) {
+class Hunt_location(var id: Int, var latitude: Double, var longitude: Double, var img_bitmap: Bitmap) {
 
 
 
@@ -13,11 +13,22 @@ class Hunt_location(var latitude: Double, var longitude: Double, var img_bitmap:
         var lat_p1 = latitude;
         var lon_p1 = longitude;
 
+        val loc1 = Location("Point A")
+        loc1.latitude = lat_p1
+        loc1.longitude = lon_p1
 
+        val loc2 = Location("Point B")
+        loc2.latitude = lat_p2
+        loc2.longitude = lon_p2
 
+        val distanceInMeters = FloatArray(1)
+        Location.distanceBetween(
+            loc1.latitude, loc1.longitude,
+            loc2.latitude, loc2.longitude,
+            distanceInMeters
+        )
     }
 
-    val loc1 = loc1.latitude = startLatitude
 
     public fun get_img(): Bitmap{
 
